f781c1a7cb2bd35c13af59f4e23d9d3c101ab3ba
walksanatora <walkerffo22@hotmail.com>
2020-09-25 15:20:12 -0400

    flatpak and renammed pushing file

--- install.sh
+++ install.sh
@@ -5,15 +5,15 @@ if [ `whoami` != 'root' ]
     echo "You must be root to do this.(install packges)"
     exit
 fi
-wget  -O discord.deb  "https://discord.com/api/download?platform=linux&format=deb"
-apt install ./discord.deb
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y com.discordapp.Discord
+flatpak install -y us.zoom.Zoom
+flatpak install -y com.valvesoftware.Steam
 wget -O chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 apt install ./chrome.deb
-wget -O zoom.deb "https://zoom.us/client/latest/zoom_amd64.deb"
-apt install ./zoom.deb
 apt remove firefox
-rm zoom.deb discord.deb chrome.deb
-echo "install should be finished now press enter to exit the script"
+rm chrome.deb
 apt update
 apt install fish
 apt upgrade
