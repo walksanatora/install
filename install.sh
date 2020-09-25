1e0c595d25bea025a8d5f3f17196a2cb2b4e96a7
walksanatora <walkerffo22@hotmail.com>
2020-09-25 14:29:05 -0400

    misc

--- install.sh
+++ install.sh
@@ -2,15 +2,21 @@
 
 if [ `whoami` != 'root' ]
   then
-    echo "You must be root to do this.(install pacakges)"
+    echo "You must be root to do this.(install packges)"
     exit
 fi
 wget  -O discord.deb  "https://discord.com/api/download?platform=linux&format=deb"
-dpkg -i discord.deb
+apt install ./discord.deb
 wget -O chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-dpkg -i chrome.deb
+apt install ./chrome.deb
 wget -O zoom.deb "https://zoom.us/client/latest/zoom_amd64.deb"
-dpkg -i zoom.deb
+apt install ./zoom.deb
 rm zoom.deb discord.deb chrome.deb
 echo "install should be finished now press enter to exit the script"
+apt update
+apt install fish
+apt upgrade
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
+chsh $(logname) -s /usr/local/bin/fish
+echo "install finished press enter to exit"
 read
