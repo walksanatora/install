48b6dcb5a62b857688a241cbce6d8b70bd843e15
walksanatora <walkerffo22@hotmail.com>
2020-09-25 15:50:11 -0400

    removed flatpack zoom heading back to .deb

--- install.sh
+++ install.sh
@@ -8,13 +8,14 @@ fi
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y com.discordapp.Discord
-flatpak install -y us.zoom.Zoom
 flatpak install -y com.valvesoftware.Steam
 flatpak install -y com.obsproject.Studio
 wget -O chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 apt install ./chrome.deb
 apt remove firefox
-rm chrome.deb
+wget -O zoom.deb "https://zoom.us/client/latest/zoom_amd64.deb"
+apt install ./zoom.deb
+rm chrome.deb zoom.deb
 apt update
 apt install fish
 apt upgrade
