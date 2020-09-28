8361d46e13a767b1c9306b9506efa0654bf21cdd
walksanatora <walkerffo22@hotmail.com>
2020-09-28 12:09:13 -0700

    smth idk i forgot

--- install.sh
+++ install.sh
@@ -3,23 +3,24 @@
 if [ `whoami` != 'root' ]
   then
     echo "You must be root to do this.(install packges)"
-    exit
+    exit 1
 fi
-
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install -y com.discordapp.Discord
-flatpak install -y com.valvesoftware.Steam
-flatpak install -y com.obsproject.Studio
+apt update
+wget -O discord.deb "https://discord.com/api/download?platform=linux"
+apt install ./discord.deb
+apt install steam
+apt install obs-studio
+apt install -y fish
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
+chsh $(logname) -s /usr/local/bin/fish
+apt install apache2
 wget -O chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-apt install ./chrome.deb
+apt install -y ./chrome.deb
 apt remove firefox
 wget -O zoom.deb "https://zoom.us/client/latest/zoom_amd64.deb"
 apt install ./zoom.deb
-rm chrome.deb zoom.deb
-apt update
-apt install fish
-apt upgrade
-echo /usr/local/bin/fish | sudo tee -a /etc/shells
-chsh $(logname) -s /usr/local/bin/fish
+rm chrome.deb zoom.deb discord.deb
+apt -y upgrade
 echo "install finished press enter to exit"
 read
+exit 0
