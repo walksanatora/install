858f22f2935d18cab3310cd56c58089ce6f86d9e
walksanatora <walkerffo22@hotmail.com>
2020-09-30 08:57:18 -0700

    comments and reordering (also apache2 + php)

--- install.sh
+++ install.sh
@@ -5,26 +5,42 @@ if [ `whoami` != 'root' ]
     echo "You must be root to do this.(install packges)"
     exit 1
 fi
-apt update
-wget -O discord.deb "https://discord.com/api/download?platform=linux"
-apt install ./discord.deb
-apt install -y steam
-apt install -y obs-studio
-apt install -y fish
+
+#gotta update that cache
+yes | apt update
+
+#misc
+yes | apt install steam
+yes | apt install obs-studio
+
+#fish and installing bobthe fish (powerline) and making t deafault
+yes | apt install fish
 echo /usr/local/bin/fish | sudo tee -a /etc/shells
 chsh $(logname) -s /usr/local/bin/fish
 curl -L https://get.oh-my.fish | fish
 echo "omf install bobthefish" | fish
 cp -a /fish/. /home/$(logname)/.config/fish/functions
 echo "source /home/$(logname)/.config/fish/functions/func.fish" >> /home/$(logname)/.config/fish/config.fish
-apt install apache2
+
+#web server stuffs
+yes | apt install apache2
+yes | apt-get install php5 libapache2-mod-php5
+
+#downloaded packages
+wget -O discord.deb "https://discord.com/api/download?platform=linux"
+yes | apt install ./discord.deb
 wget -O chrome.deb "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-apt install -y ./chrome.deb
-apt remove firefox
+yes | apt install ./chrome.deb
+yes | apt remove firefox
 wget -O zoom.deb "https://zoom.us/client/latest/zoom_amd64.deb"
-apt install ./zoom.deb
+yes | apt install ./zoom.deb
 rm chrome.deb zoom.deb discord.deb
-apt -y upgrade
+
+#everything else
+yes | apt upgrade
+
+#finished gotta let them know
 echo "install finished press enter to exit"
 read
 exit 0
+
